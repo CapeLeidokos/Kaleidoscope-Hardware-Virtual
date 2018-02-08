c333c9902c7bdf84e886734cdba1fef7be631458
Florian Fleissner <fleissner@inpartik.de>
2018-02-08 17:47:48 +0100

    Appveyor

--- src/GuardedArduino.h
+++ src/GuardedArduino.h
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "GuardedArduino.h"
+#include "Arduino.h"
 
 // Arduino.h defines some macros that pollute
 // the namespace when build for the host system.
